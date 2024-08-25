3ab08e9189e22d85561d7179ede56a5516eccba6
Karlo Prikratki <karlo@karloprikratki.com>
2024-08-25 18:08:48 +0200

    defer

--- raidtools/event_create.py
+++ raidtools/event_create.py
@@ -1120,6 +1120,7 @@ class EventSpecDropdown(discord.ui.Select):
     async def callback(self, interaction: discord.Interaction):
         log.debug(f"Spec dropdown callback for {interaction.user.name}, picked {self.values}")
         picked_spec = self.values[0]
+        await interaction.response.defer()
         # Get player's current participation status
         current_events = await self.config.guild(interaction.guild).events()
         user_events = await self.config.member(interaction.user).events()
@@ -1176,13 +1177,13 @@ class EventSpecDropdown(discord.ui.Select):
         event_msg = await interaction.channel.fetch_message(int(event_id))
         await event_msg.edit(embed=embed)
         try:
-            await interaction.response.send_message("Uspješno si se prijavio.", ephemeral=True)
+            await interaction.followup.send_message("Uspješno si se prijavio.", ephemeral=True)
         except discord.NotFound:
             # Can't respond to interaction because user deleted the ephemeral message it was invoked from.
             pass
 
     async def add_participant_to_thread(
-        self, thread_id: int, user: discord.User, guild: discord.Guild
+        self, thread_id: int, user: discord.User | discord.Member, guild: discord.Guild
     ):
         if not thread_id:
             return
